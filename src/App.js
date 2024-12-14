8e8d0459186b784685041740343e18360ba81200
manoj <manojarun4820@gmail.com>
2024-12-14 11:39:58 +0530

    Update routing in App.js to include login path

--- src/App.js
+++ src/App.js
@@ -25,8 +25,7 @@ function App() {
           <Route path="/" element={<Navigate to={"/login"} />} />
 
           <Route path="/home" element={<HomePage />} />
-  
-  
+          <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
           <Route path="/temprature" element={<TempratureLive />} />
           <Route path="/socket" element={<Socketprogram />} />
